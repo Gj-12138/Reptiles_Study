f91debc0b76ea30cc609dd813626c73cf6c3b7d0
Gj <2964820196@qq.com>
2025-10-27 17:12:23 +0800

    allow_redirects 、requests的模拟登录

--- RequestsStudy.py
+++ RequestsStudy.py
@@ -1,3 +1,4 @@
+import re
 from os import write
 
 import requests
@@ -10,7 +11,9 @@ import requests
 
 print(requests.__cake__)
 
-
+"""
+基础
+"""
 # url = "http://httpbin.org/get?name=gg"
 
 # response = requests.get(url)
@@ -32,10 +35,77 @@ print(requests.__cake__)
 # print(response.request.url)
 # print(dir(response.request))
 # print("------------------------------03")
-url = "https://contentcms-bj.cdn.bcebos.com/cmspic/bc7bfcbbdc9f43646903046238a87d2a.jpeg?x-bce-process=image/crop,x_0,y_0,w_636,h_346"
-response = requests.get(url)
-with open(f"static/test01.jpg","wb") as f:
-    f.write(response.content)
+# url = "http://httpbin.org/post?name=gg"
+# response = requests.post(url,params={"age":17,"length":"18cm"},json={"ggggg":"11111"},headers={"Content-Type":"application/json"})
+# print(response.json())
+# print(response.json()["data"])
+# print("------------------------------04")
 
-print("------------------------------03")
+# url = "https://contentcms-bj.cdn.bcebos.com/cmspic/bc7bfcbbdc9f43646903046238a87d2a.jpeg?x-bce-process=image/crop,x_0,y_0,w_636,h_346"
+# response = requests.get(url)
+# with open(f"static/test01.jpg","wb") as f:
+#     f.write(response.content)
+#
+# print("------------------------------05")
+
+
+# --------------------------------------------------
+#
+"""
+进阶
+"""
+"""
+requests.get() 请求参数
+"""
+# 创建django项目模拟目标网站
+
+url = "http://127.0.0.1:8000/admin/"
+
+# response = requests.get(url)
+# print(response.status_code)
+#
+# response = requests.get(url,allow_redirects=False) #禁止重定向
+# print(response.status_code)
+# print("--------------------------------------------01")
+# headers = {
+#     "cookie":"sessionid=zkhjjsg2afldjyg9eho6w2yrvln166mm"
+# }
+# # cookies
+# response = requests.get(url,allow_redirects=False,headers=headers)
+# print(response.status_code)
+# print(response.text)
+# print("--------------------------------------------02")
+# cookies = {
+#     "sessionid":"zkhjjsg2afldjyg9eho6w2yrvln166mm"
+# }
+# response = requests.get(url,allow_redirects=False,cookies=cookies)
+# print(response.status_code)
+# print(response.text)
+
+
+"""
+request 模拟登录
+"""
+url2 = "http://127.0.0.1:8000/admin/login/?next=/admin/"
+
+response = requests.get(url2)
+# 先通过get请求，获取cookie和csrfmiddlewaretoken
+# print(response.cookies)
+# print(response.text)
+csrfmiddlewaretoken = re.search(r'<input type="hidden" name="csrfmiddlewaretoken" value="(.*?)">',response.text).group(1)
+# print(csrfmiddlewaretoken)
+
+response = requests.post(url2, data={'csrfmiddlewaretoken': csrfmiddlewaretoken,'username':"admin",'password':123456}, cookies=response.cookies)
+# 在通过post传递数据
+print(response.text)
+
+print("--------------------------------------------01")
+session = requests.Session() #创建实例
+response = session.get(url2)
+csrfmiddlewaretoken = re.search(r'<input type="hidden" name="csrfmiddlewaretoken" value="(.*?)">',response.text).group(1)
+# 通过get获取csrfmiddlewaretoken
+response = requests.post(url2,data={'csrfmiddlewaretoken': csrfmiddlewaretoken,'username':"admin",'password':123456})
+print(response.text)
+print("--------------------------------------------02")
+
 
